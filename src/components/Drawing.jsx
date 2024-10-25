3f103bbb5a61262c9241980c4bb5e233bb39fc92
Carson <bergencarson@gmail.com>
2024-10-25 13:03:29 -0600

    always show crosshair

--- src/components/Drawing.jsx
+++ src/components/Drawing.jsx
@@ -3,8 +3,15 @@ import p5 from "p5";
 import { ReactP5Wrapper } from "@p5-wrapper/react";
 import { useEffect, useMemo, useRef, useState } from "react";
 import { twMerge } from "tailwind-merge";
-import { clamp } from './utils.js';
-import { Compass, Lock, LockOpen, PaintBrush, Palette, Trash } from "@phosphor-icons/react";
+import { clamp } from "./utils.js";
+import {
+  Compass,
+  Lock,
+  LockOpen,
+  PaintBrush,
+  Palette,
+  Trash,
+} from "@phosphor-icons/react";
 
 export default function Drawing({ unlockPhone }) {
   const divRef = useRef(null);
@@ -15,7 +22,7 @@ export default function Drawing({ unlockPhone }) {
   let toErase = false;
   let pos = {
     x: 0,
-    y: 0
+    y: 0,
   };
 
   const [divRect, setDivRect] = useState({ width: 0, height: 0 });
@@ -49,16 +56,14 @@ export default function Drawing({ unlockPhone }) {
 
         pos = newPos;
 
-        if (!drawing) {
-          let chlen = 6;
-          p5.fill(0);
-          p5.stroke("magenta");
-          p5.strokeWeight(2);
-          p5.line(pos.x - chlen, pos.y, pos.x + chlen, pos.y);
-          p5.stroke("magenta");
-          p5.strokeWeight(2);
-          p5.line(pos.x, pos.y - chlen, pos.x, pos.y + chlen);
-        }
+        let chlen = 6;
+        p5.fill(0);
+        p5.stroke("magenta");
+        p5.strokeWeight(2);
+        p5.line(pos.x - chlen, pos.y, pos.x + chlen, pos.y);
+        p5.stroke("magenta");
+        p5.strokeWeight(2);
+        p5.line(pos.x, pos.y - chlen, pos.x, pos.y + chlen);
       }
     };
     p5.deviceShaken = () => {
@@ -95,10 +100,6 @@ export default function Drawing({ unlockPhone }) {
           p5.background(255);
           shaking = false;
         }
-        // if (toErase) {
-        //   p5.background(255);
-        //   toErase = false;
-        // }
         const speed = 6;
         const max = 3;
         const dx = clamp(p5.rotationY * speed, -max, max);
@@ -121,11 +122,11 @@ export default function Drawing({ unlockPhone }) {
   };
 
   const resetApp = () => {
-    // drawing = false;
-    // shaking = false;
-    // done = false;
-    // shakeValue = 0;
-    // toErase = true;
+    drawing = false;
+    shaking = false;
+    done = false;
+    shakeValue = 0;
+    toErase = true;
   };
 
   const updateRect = () => {
@@ -145,7 +146,7 @@ export default function Drawing({ unlockPhone }) {
   useEffect(() => {
     updateRect();
     pos = { x: divRect.width / 2, y: divRect.height / 2 };
-    
+
     window.addEventListener("resize", updateRect);
     return () => {
       window.removeEventListener("resize", updateRect);
@@ -156,7 +157,10 @@ export default function Drawing({ unlockPhone }) {
     <>
       {/* Canvases */}
       <div className="w-screen h-screen flex flex-col justify-start items-center p-4 pt-16 shadow-inner">
-        <div className="w-full h-[50%] relative flex justify-center items-center" ref={divRef}>
+        <div
+          className="w-full h-[50%] relative flex justify-center items-center"
+          ref={divRef}
+        >
           <div className="absolute top-0 left-0 z-0">
             <ReactP5Wrapper sketch={drawingSketch} />
           </div>
@@ -169,15 +173,15 @@ export default function Drawing({ unlockPhone }) {
       {/* Buttons */}
       <div className="absolute right-0 top-0 z-[999] w-full h-full p-2 pb-20 flex justify-end items-end pointer-events-none">
         <div className="relative">
-          <button 
+          <button
             className="absolute bottom-4 right-20 bg-gray-100 shadow-[inset_0_-2px_4px_rgba(0.6,0.6,0.6,0.6)] rounded-full w-28 h-28 flex justify-center items-center pointer-events-auto"
             onClick={() => {
-              drawing = !(drawing)
+              drawing = !drawing;
             }}
           >
             <PaintBrush className="w-full h-full stroke-2 fill-black" />
           </button>
-          <button 
+          <button
             className="absolute bottom-0 right-4 bg-yellow-400 shadow-[inset_0_-2px_4px_rgba(0.6,0.6,0.6,0.6)] rounded-full w-16 h-16 flex justify-center items-center pointer-events-auto"
             onClick={() => {
               let boundingRect = divRef.current.getBoundingClientRect();
@@ -186,7 +190,8 @@ export default function Drawing({ unlockPhone }) {
           >
             <Compass className="w-full h-full stroke-2 fill-black" />
           </button>
-          <button className="absolute bottom-[4.5rem] right-0 bg-green-400 shadow-[inset_0_-2px_4px_rgba(0.6,0.6,0.6,0.6)] rounded-full w-20 h-20 flex justify-center items-center pointer-events-auto"
+          <button
+            className="absolute bottom-[4.5rem] right-0 bg-green-400 shadow-[inset_0_-2px_4px_rgba(0.6,0.6,0.6,0.6)] rounded-full w-20 h-20 flex justify-center items-center pointer-events-auto"
             onClick={unlockPhone}
           >
             <LockOpen className="w-full h-full stroke-2 fill-black" />
